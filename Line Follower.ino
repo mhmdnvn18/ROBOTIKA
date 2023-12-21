a3fc392d68b22480f14fb3959cdbda7a55e33647
anop <81241987+mhmdnvn18@users.noreply.github.com>
2023-12-21 13:16:07 +0700

    Update Line Follower.ino

--- Line Follower.ino
+++ Line Follower.ino
@@ -4,14 +4,13 @@
 #define sensor2 A1
 #define sensor3 A2
 
-
 AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
 
 void setup() {
   motor3.setSpeed(200);
   motor4.setSpeed(200);
- 
+
   pinMode(sensor1, INPUT);
   pinMode(sensor2, INPUT);
   pinMode(sensor3, INPUT);
@@ -19,38 +18,49 @@ void setup() {
 }
 
 void loop() {
-  int val1 = analogRead(sensor1);
-  int val2 = analogRead(sensor2);
-  int val3 = analogRead(sensor3);
-  Serial.print("Sensor 1 = ");
-  Serial.println(val1);
-  Serial.print("Sensor 2 = ");
-  Serial.println(val2);
-  Serial.print("Sensor 3 = ");
-  Serial.println(val3);
+  int value1 = digitalRead(sensor1);
+  int value2 = digitalRead(sensor2);
+  int value3 = digitalRead(sensor3);
 
-//   if (val1 <= 1 && val2 <= 1) 
-//   {
-//     // Jika cahaya terdeteksi oleh kedua sensor, maju
-//     motor3.run(BACKWARD);
-//     motor4.run(BACKWARD);
-//   } 
-//   else if (val1 <= 350 && val2 > 350) 
-//   {
-//     // Jika hanya sensor kiri yang mendeteksi cahaya, belok kiri
-//     motor3.run(FORWARD);
-//     motor4.run(BACKWARD);
-//   } 
-//   else if (val1 > 350 && val2 <= 350) 
-//   {
-//     // Jika hanya sensor kanan yang mendeteksi cahaya, belok kanan
-//     motor3.run(BACKWARD);
-//     motor4.run(FORWARD);
-//   } 
-//   else 
-//   {
-//     // Jika tidak ada sensor yang mendeteksi cahaya, berhenti
-//     motor3.run(RELEASE);
-//     motor4.run(RELEASE);
-//   }
+  Serial.print("Sensor 1: ");
+  Serial.print(value1);
+  Serial.print(" | Sensor 2: ");
+  Serial.print(value2);
+  Serial.print(" | Sensor 3: ");
+  Serial.println(value3);
+
+  // Implementasi logika kontrol motor sesuai dengan kondisi sensor
+  if (value1 == HIGH && value2 == LOW && value3 == HIGH) {
+    // Kasus maju
+    forward();
+  } else if (value1 == HIGH && value2 == HIGH && value3 == LOW) {
+    // Kasus belok kanan
+    turnRight();
+  } else if (value1 == LOW && value2 == HIGH && value3 == HIGH) {
+    // Kasus belok kiri
+    turnLeft();
+  } else {
+    // Kasus berhenti
+    stopMotors();
+  }
+}
+
+void forward() {
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}
+
+void turnRight() {
+  motor3.run(FORWARD);
+  motor4.run(BACKWARD);
+}
+
+void turnLeft() {
+  motor3.run(BACKWARD);
+  motor4.run(FORWARD);
+}
+
+void stopMotors() {
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
 }
